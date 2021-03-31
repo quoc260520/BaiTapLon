055974fa31122361a002f4a9d987b487e3a7cacf
quoc260520 <185P1063382@e.tlu.edu.vn>
2021-04-01 00:25:11 +0700

    del

--- xoasp.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once './ketnoi.php';
-    $idxoa=$_GET["IdSp"];
-    $sql="delete from sanpham where IdSp='$idxoa'";
-    $thuchien=mysqli_query($conn,$sql);
-    header('location: quantri.php?Page_layout=danhmucsp');
-?>
